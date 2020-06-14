44c65a05e5172abc00fcbb2f8cdfbef6d49dce32
Bruno Henrique <bruno@lohl.com.br>
2020-06-14 19:19:48 -0300

    dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,35 +2,78 @@
   <div class="flex-height base-padding">
     <Header />
     <div>
-      <template v-if="authenticated">
-        <router-link :to="{ name: 'dashboard'}">Dashboard</router-link>|
-        <router-link :to="{ name: 'logout'}">Sair</router-link>
-      </template>
+      <input type="text" placeholder="Pesquisar por local" />
+
+      <hr>
+
+      <!-- SECTION 1 -->
+      <div>
+        <h2>Destaques</h2>
+        <ul>
+          <li>Local para banho limpo</li>
+          <li>Caixa 24h</li>
+          <li>Refeições 24h</li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <!-- SECTION 2 -->
+      <div id="videos">
+        <template>
+          <img src alt="video_cover" />
+          <span>Dica no app</span>
+          <p>Como se alimentar melhor nas estradas sem gastar mais</p>
+        </template>
+      </div>
+
+      <hr />
+
+      <!-- SECTION 3 -->
+      <div id="adds">
+        <template>
+          <span>distancia</span>
+          <span>Nome Patrocinador</span>
+          <img src alt="add_background" />
+          <button>Saber mais</button>
+        </template>
+      </div>
+
+      <hr />
+
+      <!-- SECTION 4 -->
+      <div id="places_to_eat">
+        <template>
+          <h4>Posto Castelo Branco</h4>
+          <span id="review"></span>
+          <p>Endereço</p>
+        </template>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Header from "@/components/Header";
-  import { mapGetters } from "vuex";
-  export default {
-    name: "Home",
-    components: {
-      Header
-    },
-    methods: {
-      loadUsers() {
-        axios.get("/users").then(function(response) {
-          console.log(response.data);
-        });
-      }
-    },
-    computed: {
-      ...mapGetters({
-        authenticated: "auth/authenticated"
-      })
+import Header from "@/components/Header";
+import { mapGetters } from "vuex";
+export default {
+  name: "Home",
+  components: {
+    Header
+  },
+  methods: {
+    loadUsers() {
+      axios.get("/users").then(function(response) {
+        console.log(response.data);
+      });
     }
-  };
+  },
+  computed: {
+    ...mapGetters({
+      authenticated: "auth/authenticated"
+    })
+  }
+};
 </script>
 
 <style>
--- src/views/places/Place.vue
+++ src/views/places/Place.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="flex-height">
-    <Place class="shadow" title="Nome do Local" text="endereço do local" image="https://placehold.it/480x320" review="[0,2,2]">
-        <a><Icon icon="back"/></a>
+    <Place
+      class="shadow"
+      title="Nome do Local"
+      text="endereço do local"
+      image="https://placehold.it/480x320"
+      review="[0,2,2]"
+    >
+      <a>
+        <Icon icon="back" />
+      </a>
     </Place>
     <div class="divider"></div>
-    <div class="base-padding">      
+    <div class="base-padding">
       <div class="buttons">
         <Button>Abrir no Maps</Button>
         <Button class="link">Compartilhar</Button>
@@ -21,41 +29,83 @@
 </template>
 
 <script>
-  import Button from "@/components/Button";
-  import Icon from "@/components/Icon";
-  import Place from "@/components/PlaceHeader";
-  import ReviewTags from "@/components/ReviewTags";
-  export default {
-    name: "Home",
-    components: {
-      Button, Place,ReviewTags,Icon
-    },
-    data(){
-        return {
-            tags: [ 
-                { id: 1, nome: 'Local para banho grátis', qtd: '6' }, 
-                { id: 2, nome: 'Local para banho limpo', qtd: '5' }, 
-                { id: 3, nome: 'Boa comida', qtd: '12' }, 
-                { id: 4, nome: 'Banheiros Limpos', qtd: '5' }, 
-                { id: 5, nome: 'Caixa 24h', qtd: '18' }, 
-                { id: 6, nome: 'TV com Futebol', qtd: '20' }
-            ]
+import Button from "@/components/Button";
+import Icon from "@/components/Icon";
+import Place from "@/components/PlaceHeader";
+import ReviewTags from "@/components/ReviewTags";
+export default {
+  name: "Home",
+  components: {
+    Button,
+    Place,
+    ReviewTags,
+    Icon
+  },
+  data() {
+    return {
+      tags: {
+        good: {
+          type: "neutro",
+          title: "Pontos Neutros",
+          tags: [
+            { id: 1, nome: "Local para banho grátis", qtd: "6" },
+            { id: 2, nome: "Local para banho limpo", qtd: "5" },
+            { id: 3, nome: "Boa comida", qtd: "12" },
+            { id: 4, nome: "Banheiros Limpos", qtd: "5" },
+            { id: 5, nome: "Caixa 24h", qtd: "18" },
+            { id: 6, nome: "TV com Futebol", qtd: "20" }
+          ]
+        },
+
+        neutral: {
+          type: "good",
+          title: "Pontos Positivos",
+          tags: [
+            { id: 1, nome: "Local para banho grátis", qtd: "6" },
+            { id: 2, nome: "Local para banho limpo", qtd: "5" },
+            { id: 3, nome: "Boa comida", qtd: "12" },
+            { id: 4, nome: "Banheiros Limpos", qtd: "5" },
+            { id: 5, nome: "Caixa 24h", qtd: "18" },
+            { id: 6, nome: "TV com Futebol", qtd: "20" }
+          ]
+        },
+
+        bad: {
+          type: "bad",
+          title: "Pontos Negativos",
+          tags: [
+            { id: 1, nome: "Local para banho grátis", qtd: "6" },
+            { id: 2, nome: "Local para banho limpo", qtd: "5" },
+            { id: 3, nome: "Boa comida", qtd: "12" },
+            { id: 4, nome: "Banheiros Limpos", qtd: "5" },
+            { id: 5, nome: "Caixa 24h", qtd: "18" },
+            { id: 6, nome: "TV com Futebol", qtd: "20" }
+          ]
         }
-    }
-  };
+      },
+      place: {
+        id: 1,
+        name: "",
+        address: "",
+        image: "",
+        google_maps_url: ""
+      }
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .buttons{
-    display: flex;
-    flex-direction: row;
+.buttons {
+  display: flex;
+  flex-direction: row;
 
-    &>*{
-      flex: 1;
+  & > * {
+    flex: 1;
 
-      & + *{
-        margin-left: 10px;
-      }
+    & + * {
+      margin-left: 10px;
     }
   }
+}
 </style>
